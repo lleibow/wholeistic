ead7b15aabe72f3099c5d94a6a1df021a945414b
Alex Flint <me@alexf.ca>
2017-03-07 22:02:07 -0500

    fixed empty pantry page

--- app/assets/javascripts/foods.js
+++ app/assets/javascripts/foods.js
@@ -3,6 +3,9 @@ $(function() {
     event.preventDefault();
     $('.pantry-list').css('display', 'block');
     $('.food-list').css('display', 'none')
+    $('.pantry-list').css('display', 'block');
+    $('.pantry-list').css('width', '100%');
+    $('.empty-list').css('display', 'none');
     $('.pantry-tab').css('border-top', '2px solid #26bb7f');
     $('.pantry-tab').css('border-bottom', '2px solid #26bb7f');
     $('.pantry-tab').css('border-right', '2px solid #26bb7f');
@@ -13,7 +16,9 @@ $(function() {
   $('.food-list-tab').click(function(event){
     event.preventDefault();
     $('.food-list').css('display', 'block');
-    $('.pantry-list').css('display', 'none')
+    $('.pantry-list').css('display', 'none');
+    $('.pantry-list').css('display', 'none');
+    $('.empty-list').css('display', 'block');
     $('.food-list-tab').css('border-top', '2px solid #26bb7f');
     $('.food-list-tab').css('border-bottom', '2px solid #26bb7f');
     $('.food-list-tab').css('border-right', '2px solid #26bb7f');
