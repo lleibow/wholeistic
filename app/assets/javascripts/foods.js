0bc07d9cbb092d158b9cd3e9a67e4febf5f6ccd7
joshkestenberg <joshkestenberg@gmail.com>
2017-03-07 23:31:04 -0500

    refactored html tags, css, and js so that pantry list and main list are ids and class for both is main list, so styling is the same

--- app/assets/javascripts/foods.js
+++ app/assets/javascripts/foods.js
@@ -1,10 +1,8 @@
 $(function() {
   $('.pantry-tab').click(function(event){
     event.preventDefault();
-    $('.pantry-list').css('display', 'block');
-    $('.food-list').css('display', 'none')
-    $('.pantry-list').css('display', 'block');
-    $('.pantry-list').css('width', '100%');
+    $('#pantry-list').css('display', 'block');
+    $('#main-list').css('display', 'none');
     $('.empty-list').css('display', 'none');
     $('.pantry-tab').css('border-top', '2px solid #26bb7f');
     $('.pantry-tab').css('border-bottom', '2px solid #26bb7f');
@@ -15,9 +13,8 @@ $(function() {
 
   $('.food-list-tab').click(function(event){
     event.preventDefault();
-    $('.food-list').css('display', 'block');
-    $('.pantry-list').css('display', 'none');
-    $('.pantry-list').css('display', 'none');
+    $('#main-list').css('display', 'block');
+    $('#pantry-list').css('display', 'none');
     $('.empty-list').css('display', 'block');
     $('.food-list-tab').css('border-top', '2px solid #26bb7f');
     $('.food-list-tab').css('border-bottom', '2px solid #26bb7f');
