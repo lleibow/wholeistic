d12877355656716fff538cb670a05b780d948605
Alex Flint <me@alexf.ca>
2017-03-13 11:53:11 -0400

    still trying to fix the search results error

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -7,7 +7,7 @@ class FoodsController < ApplicationController
       @user = current_user
       food_api_results = Food.food_api_results(food_search_params[:name])
 
-      if food_api_results.nil?
+      if food_api_results.empty?
         redirect_to root_path
 
       elsif food_api_results["common"] == []
